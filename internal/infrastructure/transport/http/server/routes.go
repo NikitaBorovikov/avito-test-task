b08f75753420ff564737085ca03e01265a2b74e6
nikborovikov <nikborovikov@team.amocrm.com>
2025-11-16 13:42:23 +0300

    added base middleware

--- internal/infrastructure/transport/http/server/routes.go
+++ internal/infrastructure/transport/http/server/routes.go
@@ -3,11 +3,17 @@ package server
 import (
 	"avitoTestTask/internal/infrastructure/transport/http/handlers"
 
+	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
 )
 
 func initRoutes(h *handlers.Handlers) *chi.Mux {
 	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 
 	r.Get("/users/getReview", h.GetUserReviewPR)
 	r.Post("/users/setIsActive", h.SetUserActive)
